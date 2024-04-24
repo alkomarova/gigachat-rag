825584a266d87f4a35d4d44ee59fd0637cb9367a
geniusserg <danilovsergeydmitrievich@gmail.com>
2024-04-24 21:17:50 +0300

    Pdf parser PyMuPDF

---
+++ pdf_parser.py
@@ -0,0 +1,21 @@
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import MarkdownTextSplitter
+from pdf4llm.helpers import pymupdf_rag 
+import fitz
+
+pdf_path = "article_examples/article_example.pdf"
+
+# переводит просто в текст
+loader = PyMuPDFLoader(pdf_path)
+chunks = loader.load()
+with open(f"pdf_parsed_txt.txt", "w", encoding="utf-8") as f:
+    for chunk in chunks:
+        f.write(chunk.page_content)
+
+
+# тут в красивый маркдаун переводит
+doc = fitz.open(pdf_path)
+md_text = pymupdf_rag.to_markdown(doc) 
+output = open(f"pdf_parsed_markdown.md", "w", encoding="utf-8")
+output.write(md_text)
+output.close()
