d5634b320c896937863ba7c57a05939958552875
alkomarova <alenakomarova09@gmail.com>
2024-04-25 01:49:30 +0300

    add folder creation

--- pdf_parser.py
+++ pdf_parser.py
@@ -4,6 +4,10 @@ from pdf4llm.helpers import pymupdf_rag
 import fitz
 import os
 
+parsed_path = 'parsed_data/'
+if not os.path.exists(parsed_path):
+    os.makedirs(parsed_path)
+
 
 def pdf_to_txt(path: str) -> None:
     for filename in os.listdir(path):
@@ -12,7 +16,7 @@ def pdf_to_txt(path: str) -> None:
             loader = PyMuPDFLoader(file_path)
             chunks = loader.load()
             new_filename = filename.replace('.pdf', '') + '.txt'
-            with open(f"parsed_data/{new_filename}", "w", encoding="utf-8") as f:
+            with open(f"{parsed_path}{new_filename}", "w", encoding="utf-8") as f:
                 for chunk in chunks:
                     f.write(chunk.page_content)
 
@@ -24,5 +28,5 @@ def pdf_to_md(path: str) -> None:
             doc = fitz.open(file_path.path)
             md_text = pymupdf_rag.to_markdown(doc)
             new_filename = filename.replace('.pdf', '') + '.md'
-            with open(f"parsed_data/{new_filename}", "w", encoding="utf-8") as f:
+            with open(f"{parsed_path}{new_filename}", "w", encoding="utf-8") as f:
                 f.write(md_text)
