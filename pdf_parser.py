8f4fc9d517236bb25dcfde8ef4cc8df53b8f5111
geniusserg <danilovsergeydmitrievich@gmail.com>
2024-04-24 21:46:35 +0300

    3 new pdfs

--- pdf_parser.py
+++ pdf_parser.py
@@ -3,12 +3,14 @@ from langchain.text_splitter import MarkdownTextSplitter
 from pdf4llm.helpers import pymupdf_rag 
 import fitz
 
-pdf_path = "article_examples/article_example.pdf"
+pdf_path = "article_examples/article_example_3.pdf"
+
+pdf_name = pdf_path.split("/")[-1].split(".")[0]
 
 # переводит просто в текст
 loader = PyMuPDFLoader(pdf_path)
 chunks = loader.load()
-with open(f"pdf_parsed_txt.txt", "w", encoding="utf-8") as f:
+with open(f"{pdf_name}_parsed_txt.txt", "w", encoding="utf-8") as f:
     for chunk in chunks:
         f.write(chunk.page_content)
 
@@ -16,6 +18,6 @@ with open(f"pdf_parsed_txt.txt", "w", encoding="utf-8") as f:
 # тут в красивый маркдаун переводит
 doc = fitz.open(pdf_path)
 md_text = pymupdf_rag.to_markdown(doc) 
-output = open(f"pdf_parsed_markdown.md", "w", encoding="utf-8")
+output = open(f"{pdf_name}_parsed_markdown.md", "w", encoding="utf-8")
 output.write(md_text)
 output.close()
