edc77f7c8b3d3cd4d7bfac6a21426a643e48354f
geniusserg <danilovsergeydmitrievich@gmail.com>
2024-04-26 02:35:51 +0300

    GET with index request

--- web.py
+++ web.py
@@ -1,9 +1,13 @@
 from chain import get_chat_response
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_index():
+    return render_template("./ui.html")
 
 @app.route('/', methods=['POST'])  # Указываем метод POST для данного маршрута
 def get_random_number():
